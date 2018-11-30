06c32382ddb54beb1bbb983c9efe7c3c15a6edbd
Evan Warburton <123onevan@live.com>
2018-11-29 21:11:28 -0800

    Made comments pull information from database

--- src/php/item.php
+++ src/php/item.php
@@ -10,12 +10,19 @@
 </head>
 <body>
 <?php
+	//php files for pulling 
 	include "accessProduct.php";
+		
+		
 	$pid = $_GET["itemID"];
 	$itemArray = returnItem($pid);
+	//retrieve comments
+	$commentsArray = returnComments($pid);
 
 	//CHANGE THIS TO CHANGE SIMILAR ITEMS. MUST SET 4 ITEMS
 	$similarItems = returnMultipleItems(array(5, 22, 19, 7))
+	
+	
 ?>
  <?php include 'header.php';?>
 
@@ -31,21 +38,17 @@
 	</div>
 	<div class="comments">
 		<h2>Comments</h2>
-		<div class="panel">
-			<img src="../images/profilepictures/tweedledee.jpg" alt="tweedledeeProfile">
-			<h3 class="username">tweedleDee</h3>
-			<p class="ptext">Man this tea sucks. It tasted like roasted dog food</p>
-		</div>
-		<div class="panel">
-			<img src="../images/profilepictures/tweedledum.jpg" alt="tweedledumProfile">
-			<h3 class="username">tweedleDum</h3>
-			<p class="ptext">Man this tea is delicious! It tasted like roasted dog food</p>
-		</div>
-		<div class="panel">
-			<img src="../images/profilepictures/cinderella.jpg" alt="cinderellaProfile">
-			<h3 class="username">cinderella</h3>
-			<p class="ptext">Help me I can't feel my legs</p>
-		</div>
+		<?php 
+			echo "<p>".$commentsArray[0][0]."</p>";
+			foreach ($commentsArray as $value){
+				$userInfo = returnCustomer($value[2]);
+				echo "<div class=\"panel\">
+					<h3 class=".$userInfo[2].">tweedleDee</h3>
+					<p class=\"ptext\">".$value[3]."</p>
+				</div>";
+				
+			}
+		?>
 		<form action = 'comment.php' method = 'get' id = 'mainForm'>
 			<input type = 'text' name = 'comment' value = 'Enter Comment' id='commentBox'>
 			<input type = 'submit' class = 'commentBtn' name = 'commentSubmit' value = 'Submit'>
@@ -54,50 +57,22 @@
 
 	<div class="similarItems">
 		<h2>Similar Items</h2>
-		<form method="get" action="item.php" id="addSubmit">
-		<div class = "panel">
-			<figure class="itemFig">
-				<img src="<?php echo $similarItems[0][4];?>" alt="<?php echo $similarItems[0][1];?>">
-				<input type="hidden" name="itemID" value="<?php echo $similarItems[0][0];?>" />
-				<figcaption>
-					<input type="submit" class="viewItem" value="<?php echo $similarItems[0][1];?>" name="<?php echo $similarItems[0][1];?>">
-				</figcaption>
-			</figure>
-		</div>
-		</form>
-		<form method="get" action="item.php" id="addSubmit">
-		<div class = "panel">
-			<figure class="itemFig">
-				<img src="<?php echo $similarItems[1][4];?>" alt="<?php echo $similarItems[1][1];?>">
-				<input type="hidden" name="itemID" value="<?php echo $similarItems[1][0];?>" />
-				<figcaption>
-					<input type="submit" class="viewItem" value="<?php echo $similarItems[1][1];?>" name="<?php echo $similarItems[1][1];?>">
-				</figcaption>
-			</figure>
-		</div>
-		</form>
-		<form method="get" action="item.php" id="addSubmit">
-		<div class = "panel">
-			<figure class="itemFig">
-				<img src="<?php echo $similarItems[2][4];?>" alt="<?php echo $similarItems[2][1];?>">
-				<input type="hidden" name="itemID" value="<?php echo $similarItems[2][0];?>" />
-				<figcaption>
-					<input type="submit" class="viewItem" value="<?php echo $similarItems[2][1];?>" name="<?php echo $similarItems[2][1];?>">
-				</figcaption>
-			</figure>
-		</div>
-		</form>
-		<form method="get" action="item.php" id="addSubmit">
-		<div class = "panel">
-			<figure class="itemFig">
-				<img src="<?php echo $similarItems[3][4];?>" alt="<?php echo $similarItems[3][1];?>">
-				<input type="hidden" name="itemID" value="<?php echo $similarItems[3][0];?>" />
-				<figcaption>
-					<input type="submit" class="viewItem" value="<?php echo $similarItems[3][1];?>" name="<?php echo $similarItems[3][1];?>">
-				</figcaption>
-			</figure>
-		</div>
-		</form>
+		<?php
+		foreach($similarItems as $value){
+			echo "<form method=\"get\" action=\"item.php\" id=\"addSubmit\">
+			<div class = \"panel\">
+				<figure class=\"itemFig\">
+					<img src=\"".$value[4] ."\" alt=\"".$value[1]."\">
+					<input type=\"hidden\" name=\"itemID\" value=\"".$value[0]."\" >
+					<figcaption>
+						<input type=\"submit\" class=\"viewItem\" value=\"".$value[1]."\" name=\"".$value[1]."\">
+					</figcaption>
+				</figure>
+			</div>
+			</form>";	
+		}
+		?>
+
 	</div>
 
   <?php include 'footer.php'?>
