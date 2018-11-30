f445c86d0af85a259774e0e45a03cbc4f1398479
Evan Warburton <123onevan@live.com>
2018-11-29 21:09:42 -0800

    added more  utility to access product

--- src/php/accessProduct.php
+++ src/php/accessProduct.php
@@ -1,7 +1,6 @@
 <?php
-//connection information
-//CHANGE THIS INFORMATION BASED ON DATABASE INFORMATION!!!
 
+//connection information
 include "db_credential.php";
 
 
@@ -52,6 +51,59 @@ function returnMultipleItems($idArray){
 		mysqli_close($connection);
 		//return an array of product arrays
 		return $returnArray;
+	}
+	//return an array with a single value which is an error message
+	$errorarray[] = "Could not find " . (string)$pid;
+	return $errorarray;
+}
+
+//Return a list that includes all comments for a product id
+function returnComments($pid){
+	//connect to database
+	global $host, $user, $password, $database;
+	$connection = mysqli_connect($host, $user, $password, $database);
+	//check for errors
+	$error = mysqli_connect_error();
+	$errorarray = array();
+	if($error != null) {
+		//return an array with a single value which is an error message
+		$errorarray[] = "Failed to connect to database";
+		return $errorarray;
+	} else {
+		//return an array which contains all the values of the product
+		$sql = "SELECT * FROM userComments WHERE pid = '$pid'";
+		$result = mysqli_query($connection, $sql);
+		$returnArray;
+		while ($row = mysqli_fetch_row($result)){
+			$returnArray[] = $row;
+		}
+		mysqli_close($connection);
+		return $returnArray;
+	}
+	//return an array with a single value which is an error message
+	$errorarray[] = "Could not find " . (string)$pid;
+	return $errorarray;
+}
+
+//return list that contains all  information for a customer based on cid
+function returnCustomer($cid){
+	//connect to database
+	global $host, $user, $password, $database;
+	$connection = mysqli_connect($host, $user, $password, $database);
+	//check for errors
+	$error = mysqli_connect_error();
+	$errorarray = array();
+	if($error != null) {
+		//return an array with a single value which is an error message
+		$errorarray[] = "Failed to connect to database";
+		return $errorarray;
+	} else {
+		//return an array which contains all the values of the product
+		$sql = "SELECT * FROM customer WHERE cid = '$cid'";
+		$result = mysqli_query($connection, $sql);
+		$row = mysqli_fetch_row($result);
+		mysqli_close($connection);
+		return $row;
 	}
 	//return an array with a single value which is an error message
 	$errorarray[] = "Could not find " . (string)$pid;
